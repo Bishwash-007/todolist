37bc79efca2e848990e1303aa4e3be57076f8227
Bishwashdrew <drewwbishwash@gmail.com>
2024-06-16 22:02:27 +0545

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Bishwash Chaudhari on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
